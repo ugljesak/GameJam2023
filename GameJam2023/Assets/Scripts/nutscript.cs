cc84b0832f17993a0a068980fe977e888eea95e2
Veliki5382 <kuzmanov03@gmail.com>
2023-07-08 23:40:38 +0200

    nonutnovember

--- GameJam2023/Assets/Scripts/nutscript.cs
+++ GameJam2023/Assets/Scripts/nutscript.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public class nutscript : MonoBehaviour
 {
+	static int nutCount; 
+
     void Start()
     {
         
     }
 
-    
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		print("UVACEN SI BATO BRE");
+		if(collision.gameObject.tag == "Player")
+		{
+			nutCount++;
+			Destroy(gameObject);
+			Instantiate(gameObject, Vector3.zero, Quaternion.identity);
+		}
+	}
 }
