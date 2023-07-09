051e6ba4f2623ab5030046873462b9cb4ab61235
Veliki5382 <kuzmanov03@gmail.com>
2023-07-09 16:25:05 +0200

    sawblade sound.

--- GameJam2023/Assets/Scripts/sawbladescript.cs
+++ GameJam2023/Assets/Scripts/sawbladescript.cs
@@ -37,9 +37,9 @@ public class sawbladescript : MonoBehaviour
 
 	private void OnCollisionEnter2D(Collision2D collision)
 	{
-
         if(collision.gameObject.tag == "Wall")
 		{
+            print("FEF");
             gameObject.GetComponent<AudioSource>().Play();
             currentVelocity = rb.velocity;
 		}
